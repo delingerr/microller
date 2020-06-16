39333d01e49a646b1d5bee8a7e817a226656b97a
delingerr <61998410+delingerr@users.noreply.github.com>
2020-06-16 18:26:52 -0400

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers_db.burgers (name) VALUES ("Cheese Burger");
+INSERT INTO burgers_db.burgers (name) VALUES ("Bacon Cheese Burger");
+INSERT INTO burgers_db.burgers (name) VALUES ("Veggie Burger");
